af59ee193e2ba0d1db3eb4b4bed3cb0d28d1357f
Mira <athes01@gmail.com>
2023-03-14 09:46:55 +0100

    trying .env variables

--- src/routes/api/chatgpt/+server.ts
+++ src/routes/api/chatgpt/+server.ts
@@ -1,10 +1,10 @@
 import { error } from '@sveltejs/kit';
 import { ChatGPTAPI } from 'chatgpt';
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const api = new ChatGPTAPI({
-    apiKey: process.env['OPENAPI_KEY']
+    apiKey: process.env.OPENAPI_KEY
 });
  
 /** @type {import('./$types').RequestHandler} */
