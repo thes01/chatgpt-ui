e5352a981d08ab714ca7586ef6184c93ed422aa0
Mira <athes01@gmail.com>
2023-03-15 19:49:38 +0100

    debug api key

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import "../app.css";
+    const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
     let resp = "";
     let prompt = "";
 </script>
+
+{ apiKey }
 
 <p>Prompt: </p>
 <input type="text" bind:value={prompt} class="border-black border-2 w-full">
--- src/routes/api/chatgpt/+server.ts
+++ src/routes/api/chatgpt/+server.ts
@@ -9,17 +9,19 @@ const api = new ChatGPTAPI({
  
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
-  const query = url.searchParams.get('query');
-  if (!query) {
-    throw error(400, 'Missing query');
-  }
+  // const query = url.searchParams.get('query');
+  // if (!query) {
+  //   throw error(400, 'Missing query');
+  // }
 
-  const response = await api.sendMessage(query, {
-    systemMessage: `You are ChatGPT, a large language model trained by OpenAI. You answer as concisely as possible for each response
-    If you are generating a list, do not have too many items.
-    Current date: ${new Date().toISOString()}\n\n`,
-    onProgress: (partialResponse) => console.log(partialResponse.text)
-  });
+  // const response = await api.sendMessage(query, {
+  //   systemMessage: `You are ChatGPT, a large language model trained by OpenAI. You answer as concisely as possible for each response
+  //   If you are generating a list, do not have too many items.
+  //   Current date: ${new Date().toISOString()}\n\n`,
+  //   onProgress: (partialResponse) => console.log(partialResponse.text)
+  // });
  
-  return new Response(JSON.stringify(response));
+  // return new Response(JSON.stringify(response));
+
+  return new Response(apiKey);
 }
